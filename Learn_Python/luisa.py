6697187001fca74ab718ec896855babfade48182
Gryllidae-Ubuntu <acostagrillo@outlook.com>
2024-03-29 21:31:35 +0100

    Cambios ubuntu

---
+++ Learn_Python/luisa.py
@@ -0,0 +1,13 @@
+# Imprime un mensaje de bienvenida
+print("Hola, soy Luisa")
+
+# Solicita al usuario ingresar los números
+asuma = input("Ingrese el primer número: ")
+bsuma = input("Ingrese el segundo número: ")
+
+# Convierte las entradas a números enteros
+asuma = int(asuma)
+bsuma = int(bsuma)
+
+# Imprime la suma de los dos números
+print("La suma es:", asuma + bsuma)
